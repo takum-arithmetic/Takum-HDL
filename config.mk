da2a18fef1b7b79279767112129b9b0861b8d18c
Laslo Hunhold <hunhold@uni-koeln.de>
2025-08-21 23:27:08 +0200

    Simplify config.mk
    There were some remnants from older times.
    
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

--- config.mk
+++ config.mk
@@ -1,16 +1,5 @@
 # See LICENSE file for copyright and license details
 # Customize below to fit your system
 
-# suffix for binary files
-BINSUFFIX =
-
-# flags
-CPPFLAGS = -D_DEFAULT_SOURCE
-CFLAGS   = -std=c99 -Os -Wall -Wextra -Wpedantic
-LDFLAGS  = -s
-LDLIBS   =
-GHDLFLAGS = --std=93
-
 # tools
 VSG = vsg
-GHDL = ghdl
